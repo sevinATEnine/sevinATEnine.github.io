b7921b49aca0df429644680aa0a0e384733f4af3
sevinATEnine <106199818+sevinATEnine@users.noreply.github.com>
2023-05-07 12:16:48 -0600

    Added file ScratchTest.php

---
+++ scratchtest.php
@@ -0,0 +1,3 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<?php echo file_get_contents("http://clouddata.scratch.mit.edu/logs?projectid=176090680&limit=15&offset=0"); ?>
