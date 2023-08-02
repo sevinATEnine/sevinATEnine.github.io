89f4721f6ca0a056215539ab727ed61ccb453758
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-08-02 10:10:57 -0600

    I hope things aren't overwritten

--- terminal.js
+++ terminal.js
@@ -28,7 +28,7 @@ function downloadResource(url, filename) {
 let names = {
   "c@d3N":"ThatGuyOverThere",
   "$|m0n":"TacoMan",
-  "70DD":"yes",
+  "70|)|)":"yes",
   "GUesT_1.0":"guest",
   "$@wy3|-":"Sawyer",
   "root":"ROOT",
@@ -36,19 +36,29 @@ let names = {
   'm0m': 'Wirz\'s Mom',
   'd@d': 'Wirz\'s Dad',
 }//basic name definitions
-
+ var aliases = {};
 var permitted = window.sessionStorage.getItem('permittedTerminalCST');
 if (permitted != 'affirmed') {
   document.getElementById('body').style.display = 'none';
   alert("Sorry, but you do not have permission to use the cst terminal. Please use the sign-in on our home page to gain access.");
   // location.replace("./index.html");
-}else {
+}
+else if (localStorage.getItem("lockdownCST") != null && localStorage.getItem("lockdownCST") != undefined) {
+  if(prompt("Enter lockdown shuttoff key:"==localStorage.getItem("lockdownCST"))) {
+    alert("Access granted. Reload this page.")
+  }else {
+    alert("Access denied.");
+    location.href = "about:blank";
+  }
+}
+else {
   document.getElementById("prompt").textContent = "CST/"+names[sessionStorage.getItem("userTerminalCST")]+"-->";
 }//Access granted? Time to find out!
 
 var command = document.getElementById("command");
 var prev = document.getElementById("previous");
 var cmdSplit = null;
+var execWindow = [];
 //Just some DOM nodes
 
 function doCommand() {
@@ -56,23 +66,21 @@ function doCommand() {
   cmdSplit = command.value.split(" ");
   switch (cmdSplit[0]) {
     case "help": {
-      output.innerHTML = "<ul><li>ranks: Shows list of ranks<li>help: Shows list of basic commands<li>docs: Shows all commands<li>credits: Shows credits<li>quit or exit: Logs out of CST<li>kill: Kills the terminal and forwards to an empty page<li>clear: CLears the terminal<li>admin: Enters the root user</ul>";
-      output.className = "output";
-      break;
-    }
-    case "ranks": {
-      output.innerHTML = "<ul><li>0 | Root<li>1 | Owner<li>2 | Developer<li>3 | Admin<li>4 | Helper<li>5 | Icon<li>6 | Geek<li>7 | User<li>8 | Guest<li>9 | Banned</ul>";
-      output.className = "output";
-      break;
-    }
-    case "users": {
-      output.innerHTML = "<ul><li>c@d3N | Developer<li>$|m0n | Developer<li>70DD | Developer<li>GUesT_1.0 | Guest<li>root | Root<li>$@wy3|- | User<li>c2@r@ | User<li>m0m | User<li>d@d | User<li>";
+      output.innerHTML = "<ul><li>help: Shows list of basic commands<li>docs: Shows all commands<li>credits: Shows credits<li>quit or exit: Logs out of CST<li>kill: Kills the terminal and forwards to an empty page<li>clear: CLears the terminal<li>admin: Enters the root user</ul>";
       output.className = "output";
       break;
     }
     case "quit": {
       location.replace("./index.html");
       sessionStorage.setItem("permittedTerminalCST","loggedOut");
+      break;
+    }
+    case "alias": {
+      aliases[cmdSplit[1]] = cmdSplit[2];
+      break;
+    }
+    case "exec": {
+      output.innerHTML = execWindow[execWindow.length-1];
       break;
     }
     case "credits": {
@@ -106,35 +114,39 @@ function doCommand() {
       break;
     }
     case "admin": {
-      output.innerHTML="<img src='./rickroll.gif'>";
+      output.innerHTML="<img src='https://github.com/sevinATEnine/cst-command-line/blob/main/rickroll.gif?raw=true'>";
       break;
     }
     case "download": {
       downloadResource(cmdSplit[1]);
-      break;
+      break; 
     }
     case "lockdown": {
       if (sessionStorage.getItem("userTerminalCST")=="root") {
-        output.innerHTML = "Lockdown mode activated";
-        output.className = "important";
+        output.textContent = "Lockdown mode activated.";
+        output.className = "important"
+        localStorage.setItem("lockdownCST",cmdSplit[1]);
       }else {
-        output.innerHTML = "Error 02: Failed to activate lockdown mode with non root user privelages";
+        output.textContent = "Error 02: User lacking root priveleges.";
         output.className = "error";
       }
       break;
     }
-
-    case "release-lockdown": {
-      if (sessionStorage.getItem("userTerminalCST")=="root") {
-        output.innerHTML = "Lockdown mode deactivated";
-        output.className = "important";
-      }else {
-        output.innerHTML = "Error 02: Failed to deactivate lockdown mode with non root user privelages";
-        output.className = "error";
+    case "getalias": {
+      output.textContent = aliases[cmdSplit[1]];
+      break;
+    }
+    case "theme": {
+      if (document.body.style.backgroundColor == "black") {
+        document.getElementById("body").style.backgroundColor = "white";
+        command.style.backgroundColor = "white";
+      }
+      else {
+        document.getElementById("body").style.backgroundColor = "black";
+        command.style.backgroundColor = "black";
       }
       break;
     }
-
     default: {
       output.innerHTML = "Error 01: Invalid command";
       output.className = "error";
