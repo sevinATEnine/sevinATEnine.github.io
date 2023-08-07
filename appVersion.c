6fa85ec6d96e87f3f7bfed4e9de6bcc8863e5045
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-08-06 20:59:13 -0600

    Update appVersion.c

--- appVersion.c
+++ appVersion.c
@@ -1,1 +1,13 @@
-//I learned C recently, and can create another version of CST
+#include <stdio.h>
+
+struct users {
+  char username[16];
+  char password[32];
+  char name[16];
+  int id;
+};
+void main() {
+  struct users u[2] = {{"c@d3N","(0d3r_4_L1FE","ThatGuyOverThere",1},{"$|m0n","Dev","TacoMan",2}};
+ printf("\n%s\n%s\n%s\n%d\n",u[0].username,u[0].password,u[0].name,u[0].id);
+  printf("%s",u[1].username);
+};
