cb7bb37795ad1e73a67f39d9a28c21fd98f7abff
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-08-07 18:24:21 -0600

    Update appVersion.c

--- appVersion.c
+++ appVersion.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-struct users {
+void main() {
+  struct users {
   char username[16];
   char password[32];
-  char name[16];
+  char name[32];
   int id;
-};
-void main() {
-  struct users u[2] = {{"c@d3N","(0d3r_4_L1FE","ThatGuyOverThere",1},{"$|m0n","Dev","TacoMan",2}};
- printf("\n%s\n%s\n%s\n%d\n",u[0].username,u[0].password,u[0].name,u[0].id);
-  printf("%s",u[1].username);
+};//Establish user template
+
+struct users u[2] = {
+  {"c@d3N","(0d3r_4_L1FE","ThatGuyOverThere",1},
+  {"$|m0n","Dev","TacoMan",2}
+};//Create users
+  for (int i = 0;i < 2;i++) {
+    printf("\n%s\n%s\n%s\n%d\n",u[i].username,u[i].password,u[i].name,u[i].id);
+    };
 };
