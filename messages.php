680939d75a95bf6e656195dc8e3f0e6e41c50f17
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-08-27 19:45:11 -0600

    Update messages.php

--- messages.php
+++ messages.php
@@ -108,8 +108,7 @@
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
-  $sql = "SELECT username, messageContent FROM Messages";
+  $sql = "SELECT username, messageContent FROM Messages WHERE username NOT IN("#Caden-dev","#Simon-dev")";
   $result = $conn->query($sql);
 
   if ($result->num_rows > 0) {
