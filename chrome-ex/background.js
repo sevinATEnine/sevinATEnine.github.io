8d4fc9033e93addb9ff5f40ef2307f26bc9bd263
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-08-31 08:34:52 -0600

    Update background.js

--- chrome-ex/background.js
+++ chrome-ex/background.js
@@ -1,11 +1,6 @@
-var url;
-chrome.action.onClicked.addListener(async (tab) => {
-  url = tab.url;
-  var dataDiv = document.createElement("div");
-  dataDiv.innerHTML = "URL: "+url.split("&").join("&amp;").split("<").join("&lt;")+" <a onlick='hide();'>X</a>";
-  dataDiv.style = "position:absolute;left:3px;top:3px;background-color:gray;color:white;width:30%";
-  function hide() {
-    dataDiv.style.display = "none";
-  }
-  document.appendChild(dataDiv);
+chrome.action.onClicked.addListener(() => {
+  var htmlNode = document.getElementsByTagName("html")[0];
+  htmlNode = htmlNode.innerHTML;
+  document.getElementsByTagName("html")[0].textContent = "<html>"+htmlNode+"</html>";
+  //prints the HTML code of the document
 });
