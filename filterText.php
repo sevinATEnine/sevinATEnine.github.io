29d753d11cf4ecb18433559bb511cb205c5a8455
sevinATEnine <legomastersimon@icloud.com>
2023-09-03 11:28:41 -0600

    df dxg df

--- filterText.php
+++ filterText.php
@@ -2,13 +2,13 @@
 function ReplaceBadWords($comment){
     $badword = array();
     $replacementword = array();
-    $sqlOutput = array('1', '2', '3', '4');
+    $sqlOutput = array('123 4', '2', '3', '4');
     $wordlist = join("|",$sqlOutput); // replace with the list of bad words from attached rar file
     $words = explode("|", $wordlist);
     foreach ($words as $key => $word) {
     $badword[$key] = $word;
     $replacementword[$key] = addStars($word);
-    $badword[$key] = "/\b{$badword[$key]}\b/i";
+    $badword[$key] = "/{$badword[$key]}/i";
     };
     $comment = preg_replace($badword, $replacementword, $comment);
     return $comment;
