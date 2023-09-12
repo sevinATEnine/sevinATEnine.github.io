ef8127e8d3d1b825a82db7d45b4fd0a0da1df291
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-09-12 07:55:04 -0600

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -333,6 +333,14 @@ async function doCommand() {
       delete functions[cmdSplit[1]];
       break;
     }
+    case 'commit': {
+      sessionStorage.setItem("test",cmdSplit[1]);
+      break;
+    }
+    case 'retrieve': {
+      output.textContent = sessionStorage.getItem("test");
+      break;
+    }
     case 'colore': {
       // animation: colore-colors 5s infinite;
       document.getElementById('body').style.animation =
