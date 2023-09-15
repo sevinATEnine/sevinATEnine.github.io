5001f18d36d2bd2a76d59f2f8408013b8ec96d69
sevinATEnine <legomastersimon@icloud.com>
2023-09-14 18:25:16 -0600

    a

--- runPython.php
+++ runPython.php
@@ -2,6 +2,8 @@
 <head>
 <title>Run my Python files</title>
 <?PHP
-echo shell_exec("python test.py 'parameter1'");
+//echo shell_exec("python test.py");
+echo (shell_exec("ls"));
+
 ?>
 </head>
---
+++ test.py
@@ -0,0 +1,16 @@
+import PHPPython
+
+
+
+
+
+
+
+
+PHPPython.Init()
+
+for digit1 in range(96):
+    PHPPython.Log(digit1)
+    PHPPython.Error("hi")
+    
+PHPPython.End()
