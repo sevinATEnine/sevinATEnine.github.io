e939119eb3bb486d8178795f2cb19b75f20c39de
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-09-23 17:01:17 -0600

    Update appVersion.c

--- appVersion.c
+++ appVersion.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
-char user[32];
-char pass[32];
-int UID;
-
-void main() {
   struct users {
   char username[16];
   char password[32];
@@ -13,10 +7,21 @@ void main() {
   int id;
 };//Establish user template
 
-struct users u[2] = {
+struct users u[3] = {
   {"c@d3N","(0d3r_4_L1FE","ThatGuyOverThere",1},
-  {"$|m0n","Dev","TacoMan",2}
+  {"$|m0n","Dev","TacoMan",2},
+  {"t","t","TEST_USER",3}
 };//Create users
+char user[32];
+char pass[32];
+int UID;
+char cmd[64];
+void runCmd() {
+    printf("CST/%s-->",u[UID].name);
+    scanf("%s",&cmd);
+    runCmd();
+}
+void main() {
     printf("Enter your username: ");
     scanf("%s",&user);
     printf("Enter your password: ");
@@ -25,6 +30,7 @@ struct users u[2] = {
     scanf("%d",&UID);
     if (strcmp(user,u[UID].username)==0 && strcmp(pass,u[UID].password)==0) {
         printf("Logged in as: %s\n",u[UID].name);
+        runCmd();
     } else {
         printf("Username and or password entered incorrectly.");
     }
