3823256837c02578c305275979bb966cd63b2cfe
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2023-10-02 17:42:19 -0600

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -112,7 +112,7 @@ function decrypt(input, key) {
   }
 }
 function writeToStack(data) {
-  localStorage.setItem("terminalStack",localStorage.getItem("terminalStack") + "\n" + data);
+  localStorage.setItem("terminalStack",localStorage.getItem("terminalStack") + "&c" + data);
 }
 function varHandle(data, len, mode = false) {
   var outputSplit = data.slice(len + 1).split('\\');
