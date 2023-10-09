b68f86672174afb24ff254866487e11fdac0d69e
sevinATEnine <legomastersimon@icloud.com>
2023-10-09 12:43:17 -0600

    Create setPicture.php

---
+++ profilePictureInterface/setPicture.php
@@ -0,0 +1,39 @@
+<?php
+
+echo "userName:(".$_POST['userName'].")<br>";
+echo "pictureData:(".$_POST['pictureData'].")<br>";
+
+
+// $servername = "localhost";
+// $username = "elem435_cst_usr";
+// $password = "#3rm|n@2";
+// $dbname = "elem435_cst";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// $sql = "UPDATE userData SET username='".$_POST['userName']."',profilePicture='".$_POST['pictureData']."',notes=(SELECT notes FROM userData WHERE username='".$_POST['userName']."') WHERE username='".$_POST['userName']."';";
+// $result = $conn->query($sql);
+
+// // echo $result;
+// echo $result;
+// $conn->close();
+
+
+?>
+<!----------------------------------------------------------------------------------------------------
+
+The following document is owned by:
+@sevinATEnine (and alt) @Cesium72 @tacocat15 and @cmsmith02
+
+It is under the Creative Commons license and may not be reproduced commercially or without
+direct permission from the authors.
+
+Other files linked to this repository, with the exception of some of the assets, also hold
+the same criteria.
+
+---------------------------------------------------------------------------------------------------->
