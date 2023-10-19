26b72e9cbeb6f054c739c6f803869c3740087523
sevinATEnine <106199818+sevinATEnine@users.noreply.github.com>
2023-10-19 10:42:42 -0600

    Update sweep.php

--- sweep.php
+++ sweep.php
@@ -1,3 +1,6 @@
 <?php 
-echo exec('for ip `seq 1 254`; do\nping -c 161.97.25.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":"6\ndone')
+echo exec(`
+          for ip \`seq 1 254\`; do
+          ping -c 161.97.25.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":"6
+          done`);
 ?>
