0c948318662b94aa4986a44b6dab26b019c28ab5
sevinATEnine <legomastersimon@icloud.com>
2023-10-28 11:37:41 -0600

    Update banIp.php

--- MessagingSystem2/banIp.php
+++ MessagingSystem2/banIp.php
@@ -1,8 +1,8 @@
 <?php
 $password = $_POST['loginPassword'];
 $username = $_POST['loginUsername'];
-if($username = 'SYSTEM_MASTER_MESSAGE_IP_ROOT_ADMIN') {
-    if($password = '3.14159265358979323846264338327950288') {
+if($username == 'SYSTEM_MASTER_MESSAGE_IP_ROOT_ADMIN') {
+    if($password == '3.14159265358979323846264338327950288') {
         echo 'Successfully logged in.<br>';
     } else {
         die('LOGIN_ERROR_01: Invalid auth.');
