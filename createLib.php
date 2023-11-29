1116c8ff6f31412d99d3435cac3e62d044bae59f
Caden OKellylee <bmctilton@gmail.com>
2023-11-29 08:08:51 -0700

    Added Library creator

---
+++ createLib.php
@@ -0,0 +1,7 @@
+<?php
+    $lib = fopen("./libraries/".$_POST["file"], "w") or die("Unable to open file!");
+    $txt = $_POST["contents"];
+    fwrite($lib, $txt);
+    fclose($lib);
+    echo("Library created.")
+?>
