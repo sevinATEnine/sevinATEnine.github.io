86ccd1bb0d5c7d93feb6a7d354fc99e9346b2b34
sevinATEnine <legomastersimon@icloud.com>
2023-11-29 16:51:52 -0700

    Fixed libraries

--- createLib.php
+++ createLib.php
@@ -1,5 +1,5 @@
 <?php
-    $lib = fopen("./libraries/".$_POST["file"], "w") or die("Unable to open file!");
+    $lib = fopen("./libraries/".$_POST["file"].'.cst', "w") or die("Unable to open file!");
     $txt = $_POST["contents"];
     fwrite($lib, $txt);
     fclose($lib);
