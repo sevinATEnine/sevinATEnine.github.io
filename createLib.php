6551c617e76f655d27934e5713b395ccce79ffa9
Caden OKellylee <bmctilton@gmail.com>
2023-12-02 10:14:36 -0700

    Update

--- createLib.php
+++ createLib.php
@@ -1,5 +1,5 @@
 <?php
-    $lib = fopen("./libraries/".$_POST["file"].'.cst', "w") or die("Unable to open file!");
+    $lib = fopen("./libraries/".$_POST["file"]."cst", "x+") or die("Unable to create file and/or requested library already exists.");
     $txt = $_POST["contents"];
     fwrite($lib, $txt);
     fclose($lib);
