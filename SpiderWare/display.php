d3078e3f2fd15b13c1732db48c28dbe14483cc94
sevinATEnine <legomastersimon@icloud.com>
2024-01-15 11:50:14 -0700

    Update display.php

--- SpiderWare/display.php
+++ SpiderWare/display.php
@@ -1,4 +1,34 @@
-<?php
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>Data</h2>
+
+<table>
+  <tr>
+    <th>IP</th>
+    <th>Last Updated</th>
+    <th>Json</th>
+  </tr>
+  <?php
 $servername = "localhost";
 $username = "elem435_cst_usr";
 $password = "#3rm|n@2";
@@ -17,10 +47,14 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "ip: " . $row["ip"]. " - lastUpdated: " . $row["lastUpdated"]. " - jsonData" . $row["jsondata"]. "<br>";
+    echo "<tr><td>" . $row["ip"]. "</td><td>" . $row["lastUpdated"]. "</td><td>" . $row["jsondata"]. "</td></tr>";
   }
 } else {
   echo "0 results";
 }
 $conn->close();
 ?>
+</table>
+
+</body>
+</html>
