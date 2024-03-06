f3f3c0fe10c9d186fa68c26c6b6e06d361cd2423
sevinATEnine <legomastersimon@icloud.com>
2024-03-05 18:14:29 -0700

    Update main.js

--- UserBlocker/main.js
+++ UserBlocker/main.js
@@ -3,7 +3,7 @@ var blockedUserAgents = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 ];
 
-var forbiddenHTML = '<center>403 - FORBIDDEN. Your User-Agent has been blocked! :)<br>If this is a mistake, contact the devs!<br>(its not)<br>(anyway i\'m not ginving you my email)</center>';
+var forbiddenHTML = '<center>403 - FORBIDDEN. Your User-Agent has been blocked!<br>If this is a mistake, contact the devs!</center>';
 
 
 if (blockedUserAgents.includes(window.navigator.userAgent)) {
