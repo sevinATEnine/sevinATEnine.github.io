1ec284748a1848b11c2b7bc493480d1f4ec173ff
sevinATEnine <legomastersimon@icloud.com>
2024-04-28 15:25:58 -0600

    Update report.php

--- MessagingSystem2/report.php
+++ MessagingSystem2/report.php
@@ -6,7 +6,7 @@ $message=$_GET["message"];
 $user=$_GET["user"];
 $time = date(DATE_RFC822);
 
-$to = "simon@wirz.com,bmctilton@gmail.com";
+$to = "simon@wirz.com,bmctilton@gmail.com,simon.robot312@gmail.com";
 $subject = "Reported message";
 $txt = "User $user has reported the following:\n\nMessage id: $messageId\nId: $id\nSender: $sender\nMessage: $message\nTime: $time\n\nIf you want to delete, go to this link:\nhttps://hobbyrobot.com/cst/MessagingSystem2/delete.php?id=$messageId";
 $headers = "From: CST Reports" . "\r\n";
