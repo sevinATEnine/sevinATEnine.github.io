44c1b3ec67e2cf875aedb1495d69662190492cdb
sevinATEnine <legomastersimon@icloud.com>
2024-04-29 19:38:14 -0600

    Removed email feature of report - temporatilly

--- MessagingSystem2/report.php
+++ MessagingSystem2/report.php
@@ -11,8 +11,8 @@ $subject = "Reported message";
 $txt = "User $user has reported the following:\n\nMessage id: $messageId\nId: $id\nSender: $sender\nMessage: $message\nTime: $time\n\nIf you want to delete, go to this link:\nhttps://hobbyrobot.com/cst/MessagingSystem2/delete.php?id=$messageId";
 $headers = "From: CST Reports" . "\r\n";
 
-mail($to,$subject,$txt,$headers);
+// mail($to,$subject,$txt,$headers);
 
-echo "Reported message $messageId [$id]";
+echo "Reported message $messageId [$id], but did not notify moderators.\nEncoutered dev error: 45634 - Code diabled for security reasons.";
 
 ?>
